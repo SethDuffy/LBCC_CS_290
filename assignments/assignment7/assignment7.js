1b9ab238d565df252e09ca731f85b627d3b96976
SethDuffy <nassabear@gmail.com>
2019-02-16 22:16:50 -0800

    Started js assingment

---
+++ assignments/assignment7/assignment7.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("todoButton");
+const ul = document.getElementsByTagName("ul");
+button.onclick = addItem();
+var listNumber = 0;
+
+function addItem(){
+    button.style.color = "#0a3fa0";
+    const textField = document.getElementById("textField")
+    ul.createElement("li"["words"]);
+    document.getElementsByTagName("li")
+}

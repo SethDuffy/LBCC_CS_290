69f6047e2796c896a54bc98be923592514c52cf5
Seth Duffy <nassabear@gmail.com>
2019-03-20 02:01:12 -0700

    putting in final website

---
+++ assignments/chat2.0/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const socketio = require("socket.io");
+const app = express();
+const server = app.listen(3000);
+const io = socketio(server);
+const path = require("path");
+const randomWords = require('random-words');
+
+var users = {};
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+
+io.sockets.on("connection", (socket) => {
+    users[socket.id] = randomWords({exactly:1, wordsPerString:3, separator: '-'});
+    
+
+
+    socket.on("chatRoom", (room) => {
+        socket.join(room);
+        socket.broadcast.in(room).emit("new user", users[socket.id] + " new user has joined");
+    });
+
+
+
+    socket.on("new message", (data) => {
+        io.sockets.in("chatRoom").emit('news', users[socket.id] + ": "+ data);
+    });
+
+});
+
+
+
+console.log(`server started on port ${server}`);
